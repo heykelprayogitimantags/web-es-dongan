c2e0306068c677d602e9602c4f0145a88d1815d3
heykelprayogitimantags <heykelprayogi123@gmail.com>
2025-12-11 23:08:52 +0700

    delete wishlist di user page

--- app/Views/frontend/index.php
+++ app/Views/frontend/index.php
@@ -510,12 +510,6 @@
                                     <?= $product['category_name'] ?>
                                 </span>
                             </div>
-
-                            <button class="absolute top-4 right-4 w-10 h-10 bg-white/90 rounded-full flex items-center justify-center shadow-lg hover:bg-white transition-all">
-                                <svg class="w-5 h-5 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                                </svg>
-                            </button>
                         </div>
 
                         <div class="p-6">
