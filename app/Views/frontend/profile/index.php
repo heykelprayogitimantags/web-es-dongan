1c60699729f83afa3ab5da4eceeadefdbf5ac0d6
heykelprayogitimantags <heykelprayogi123@gmail.com>
2025-12-13 09:35:02 +0700

    update profile

--- app/Views/frontend/profile/index.php
+++ app/Views/frontend/profile/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile - Toko Es</title>
+    <title>Profile Es Dongan</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gradient-to-br from-gray-50 to-cyan-50 min-h-screen">
